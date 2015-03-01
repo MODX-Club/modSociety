9895404794e6ab6955eb4124741facf58464bdfc
Nikolay Lanets <info@newpg.ru>
2015-03-01 19:25:42 +0300

    Update societyvote.class.php

--- core/components/modsociety/model/modSociety/societyvote.class.php
+++ core/components/modsociety/model/modSociety/societyvote.class.php
@@ -1,2 +1,39 @@
 <?php
-class SocietyVote extends xPDOSimpleObject {}
+class SocietyVote extends xPDOSimpleObject{
+     
+    
+    public function remove(array $ancestors= array ()) {
+         
+        $ok = parent::remove($ancestors);
+        
+        if($ok){
+            
+            if($thread = $this->Thread){
+                
+                switch($this->vote_direction){
+                    
+                    case '1':
+                        $thread->positive_votes = $thread->positive_votes - 1;
+                        break;
+                        
+                    case '-1':
+                        $thread->negative_votes = $thread->negative_votes - 1;
+                        break;
+                    default:
+                        $thread->neutral_votes = $thread->neutral_votes - 1;
+                        ;
+                    
+                } 
+                
+                $thread->rating = $thread->rating - ($this->vote_value);
+                
+                $thread->save();
+            }
+            
+        }
+        
+        
+        return $ok;
+    }
+
+}
